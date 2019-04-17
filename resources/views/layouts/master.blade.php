cac761dc9a97199ed3bb8e8168d48cba4d5f7eda
vardy <jarred.vardy@gmail.com>
2019-04-17 05:13:16 +0100

    Restructuring of blade views to use new master layout based off of testing with test route. Basic routing setup for future expansion.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,75 +3,35 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Client Portal</title>
 
-    <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<div class="container">
+    <div class="row">
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+        <div id="side-nav" class="col-lg-2 col-md-2">
 
-                    </ul>
+            <h1>Client<br>Portal</h1>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+            <div id="side-nav-body">
+                <ul>
+                    <li><span><a href="{{ route('invoices') }}">Invoices</a></span></li>
+                    <li><span><a href="{{ route('nda') }}">NDA</a></span></li>
+                    <li><span><a href="{{ route('personal_details') }}">Personal Details</a></span></li>
+                    <li><span><a href="{{ route('careers') }}">Careers</a></span></li>
+                </ul>
             </div>
-        </nav>
+        </div>
 
-        <main class="py-4">
+        <div id="content-area" class="col-lg-10 col-md-10">
             @yield('content')
-        </main>
-    </div>
+        </div>
 
-    <!-- Local JS Scripts -->
-    <script src="{{ mix('/js/manifest.js') }}"></script>
-    <script src="{{ mix('/js/vendor.js') }}"></script>
-    <script src="{{ mix('/js/app.js') }}"></script>
+    </div>
+</div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,27 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+/* Generate authentication routes for pages such as 'login', 'register', and 'logout' */
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', function () {
+    return view('invoices');
+});
+
+
+// Resource controllers
+Route::get('/invoices', 'InvoiceController@index')->name('invoices');
+Route::get('/nda', 'NDAController@index')->name('nda');
+Route::get('/personal_details', 'PersonalDetailsController@index')->name('personal_details');
+
+// Careers page redirects users to main AsiaLion site careers page
+Route::get('/careers', function() {
+    return view('careers');
+
+})->name('invoices');
+
+
+// Test route for CSS experimentation
+Route::get('/test', function() {
+   return view('test');
+});
