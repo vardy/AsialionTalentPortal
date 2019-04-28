2ea71cb51aa28462195ebcd23e3c02c927675de3
vardy <jarred.vardy@gmail.com>
2019-04-28 04:26:02 +0100

    Added fields to database table

--- database/migrations/2019_04_28_030653_create_personal_details_table.php
+++ database/migrations/2019_04_28_030653_create_personal_details_table.php
@@ -20,6 +20,32 @@ class CreatePersonalDetailsTable extends Migration
             $table->string('user_id');
 
             $table->timestamps();
+
+            $table->string('first_name')->nullable(); // Required
+            $table->string('last_name')->nullable(); // Required
+            $table->string('email')->nullable(); // Required
+            $table->string('skype_id')->nullable();
+
+            $table->string('country_of_residence')->nullable(); // Required
+            $table->string('mobile_number')->nullable(); // Required
+            $table->string('home_number')->nullable();
+
+            $table->string('highest_education')->nullable(); // Required
+            $table->string('professional_experience')->nullable(); // Required
+            $table->string('industry_experience')->nullable(); // Required
+            $table->string('language_pairs')->nullable(); // Required
+            $table->string('tools')->nullable();
+            $table->string('turnaround_per_day')->nullable();
+
+            // Permanent positions
+            $table->string('latest_remuneration')->nullable();
+            $table->string('expected_remuneration')->nullable();
+
+            // Freelancers
+            $table->string('translation_rate')->nullable();
+            $table->string('editing_rate')->nullable();
+            $table->string('transcription_rate')->nullable();
+            $table->string('hourly_rate')->nullable();
         });
     }
 
