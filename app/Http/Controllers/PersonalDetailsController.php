e7f11478553798abce3d1b962da58275d938d3c1
vardy <jarred.vardy@gmail.com>
2019-04-28 04:07:23 +0100

    Added boilerplate for PersonalDetials model, controller, factory and migration.

---
+++ app/Http/Controllers/PersonalDetailsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PersonalDetails;
+use Illuminate\Http\Request;
+
+class PersonalDetailsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PersonalDetails  $personalDetails
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PersonalDetails $personalDetails)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PersonalDetails  $personalDetails
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PersonalDetails $personalDetails)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PersonalDetails  $personalDetails
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PersonalDetails $personalDetails)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PersonalDetails  $personalDetails
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PersonalDetails $personalDetails)
+    {
+        //
+    }
+}
---
+++ database/factories/PersonalDetailsFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\PersonalDetails::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
