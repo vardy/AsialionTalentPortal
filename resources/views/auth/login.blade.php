ca9216e50275ebc0da5b82bb090dd292562befc8
vardy <jarred.vardy@gmail.com>
2019-05-12 03:53:00 +0100

    Removed CSS import for app.css to reduce load time

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,7 +17,6 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
         <!-- Local CSS -->
-        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
         <link href="{{ mix('css/login.css') }}" rel="stylesheet">
         <link href="{{ mix('css/cookie_consent.css') }}" rel="stylesheet">
     </head>
