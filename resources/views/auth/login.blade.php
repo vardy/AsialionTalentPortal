294ceab9d51c1d3b8cb29a4c943c3438bdfbb938
vardy <jarred.vardy@gmail.com>
2019-05-12 03:43:13 +0100

    Optimised login page loading time by defering JS and removing unused CSS

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,21 +7,9 @@
 
         <title>Login | Talent Portal</title>
 
-        <!-- DataTables / JQuery -->
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jq-3.3.1/jszip-2.5.0/dt-1.10.18/b-1.5.6/b-colvis-1.5.6/b-flash-1.5.6/b-html5-1.5.6/b-print-1.5.6/cr-1.5.0/r-2.2.2/sl-1.3.0/datatables.min.css"/>
-
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-        <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jq-3.3.1/jszip-2.5.0/dt-1.10.18/b-1.5.6/b-colvis-1.5.6/b-flash-1.5.6/b-html5-1.5.6/b-print-1.5.6/cr-1.5.0/r-2.2.2/sl-1.3.0/datatables.min.js"></script>
-
         <!-- Bootstrap / Popper -->
         <!-- Bootstrap -->
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/superhero/bootstrap.min.css" rel="stylesheet" integrity="sha384-LS4/wo5Z/8SLpOLHs0IbuPAGOWTx30XSoZJ8o7WKH0UJhRpjXXTpODOjfVnNjeHu" crossorigin="anonymous">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-
-        <!-- Particles.JS -->
-        <script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
 
         <!-- Local CSS -->
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
@@ -41,11 +29,11 @@
 
         <div class="vertical-centered-containers">
 
-            <div class="brand-image-container row">
+            <div class="brand-image-container">
                 <img src="/storage/files/AL_LOGO_WHITE_SCALEUP.png" alt="AsiaLion Logo" class="brand-image">
             </div>
 
-            <div id="login-form" class="login-area row">
+            <div id="login-form" class="login-area">
                 <form method="POST" action="{{ route('login') }}">
                     @csrf
 
@@ -89,16 +77,29 @@
     <script src="{{ mix('/js/manifest.js') }}"></script>
     <script src="{{ mix('/js/vendor.js') }}"></script>
     <script src="{{ mix('/js/app.js') }}"></script>
-    <script src="{{ mix('/js/particlesjs.js') }}"></script>
+    <!-- <script src="{{ mix('/js/particlesjs.js') }}"></script> -->
 
-    <script>
-        $(window).resize(function() {
-            var cw = $('.brand-image').width();
-            $('.brand-image').css({'height':cw*0.29836666667+'px'});
-        });
+    <script type="text/javascript">
+        function runParticlesOnLoad() {
+            var particlesCDN = document.createElement("script");
+            particlesCDN.src = "http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js";
 
-        var cw = $('.brand-image').width();
-        $('.brand-image').css({'height':cw*0.29836666667+'px'});
+            var particlesLocal = document.createElement("script");
+            particlesLocal.src = '{{ mix('/js/particlesjs.js') }}';
+
+            document.body.appendChild(particlesCDN);
+            document.body.appendChild(particlesLocal);
+        }
+
+        if (window.addEventListener) {
+            window.addEventListener("load", runParticlesOnLoad, false);
+
+        } else if (window.attachEvent){
+            window.attachEvent("onload", runParticlesOnLoad);
+
+        } else {
+            window.onload = runParticlesOnLoad;
+        }
     </script>
 
     @include('cookieConsent::index')
